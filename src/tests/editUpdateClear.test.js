a2910ea308429ceadecab182f65b6051a99ca664
IngDavidTM <david-1011@hotmail.com>
2022-10-06 13:06:21 -0500

    add edit and update tests

---
+++ src/tests/editUpdateClear.test.js
@@ -0,0 +1,47 @@
+import addObj from '../modules/addObj.js';
+import mainArr from '../modules/const.js';
+
+document.body.innerHTML = `<form><input type="text" id="add-input" placeholder="Add to your list...">
+<button type="button" id="add-button"><i class="fa-solid fa-paper-plane"></i></button></form>
+<div id="list" class="list"></div>`;
+
+describe('edit method', () => {
+  test('should update the List', () => {
+    addObj('hello planet');
+    const input = document.querySelector('.itemText');
+    input.value = 'hello Benjamin';
+    input.dispatchEvent(new Event('input'));
+    expect(document.querySelector('.itemText').value).toBe('hello Benjamin');
+  });
+  test('should update the mainArr', () => {
+    const input = document.querySelector('.itemText');
+    input.value = 'hello David';
+    input.dispatchEvent(new Event('input'));
+    expect(mainArr).toEqual([{
+      description: 'hello David',
+      completed: false,
+      index: 1,
+    }]);
+  });
+});
+describe('update status', () => {
+  test('should update the mainArr', () => {
+    addObj('cheers');
+    const checkbox = document.querySelectorAll('.checkbox');
+    checkbox[0].click();
+    checkbox[1].click();
+    expect(mainArr).toEqual([{
+      description: 'hello David',
+      completed: true,
+      index: 1,
+    }, {
+      description: 'cheers',
+      completed: true,
+      index: 2,
+    }]);
+  });
+  test('should update the status', () => {
+    const checkbox = document.querySelectorAll('.checkbox');
+    expect(checkbox[0].checked).toEqual(true);
+  });
+});
