cd9260f3f78f9c63a54ff4b18fa067c5afe0821f
IngDavidTM <david-1011@hotmail.com>
2022-12-09 09:38:50 -0500

    improve functionality

--- src/index.js
+++ src/index.js
@@ -5,7 +5,14 @@ import refreshBtn from './modules/refresh.js';
 import { clearFunction } from './modules/check.js';
 
 const buttonAdd = document.getElementById('add-button');
+const inputAdd = document.getElementById('add-input');
 buttonAdd.addEventListener('click', btnAdd);
+inputAdd.addEventListener('keypress', (e) => {
+  if (e.key === 'Enter') {
+    e.preventDefault();
+    btnAdd();
+  }
+});
 const refresh = document.getElementById('refresh');
 refresh.addEventListener('click', refreshBtn);
 const clear = document.getElementById('clear');
